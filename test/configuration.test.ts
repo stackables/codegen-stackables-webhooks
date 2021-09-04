63d5e2e48b938a07552b5ed54565e84cd41d2152
Aarne Laur <aarne.laur@gmail.com>
2021-09-04 14:51:55 +0200

    loader skeleton

--- test/configuration.test.ts
+++ test/configuration.test.ts
@@ -1,11 +1,31 @@
 import { getConfiguration } from '../src'
+import { buildClientSchema, getIntrospectionQuery } from 'graphql';
+import axios from 'axios'
 
 test('Configuration snapshot', () => {
     const config = getConfiguration({
-        accountSlug: 'test-account',
+        accountSlug: 'stackables-test/webhooks',
         generatedFile: './src/generated/webhooks.ts',
-        introspectionToken: 'ab68132b-a320-489b-bfa4-2c938f20784e',
+        introspectionToken: 'a89751eb-ef0d-4663-9530-fa683b9d8483',
     })
 
     expect(config).toMatchSnapshot()
+})
+
+test('Configuration introspection is working correctly', async () => {
+    const { schema } = getConfiguration({
+        accountSlug: 'stackables-test/webhooks',
+        generatedFile: './src/generated/webhooks.ts',
+        introspectionToken: 'a89751eb-ef0d-4663-9530-fa683b9d8483',
+    })
+
+    const { data } = await axios({
+        url: schema,
+        method: 'post',
+        data: {
+            query: getIntrospectionQuery()
+        }
+    })
+
+    buildClientSchema(data.data)
 })
