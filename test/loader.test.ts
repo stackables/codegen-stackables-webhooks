fc579f2326d24752ee0fce329e72818b409edf06
Aarne Laur <aarne.laur@gmail.com>
2022-03-05 09:52:33 +0100

    chore: Disable old tests

--- test/loader.test.ts
+++ test/loader.test.ts
@@ -2,7 +2,7 @@ import loader from '../src'
 
 jest.setTimeout(30000)
 
-test('Test Loader', async () => {
+test.skip('Test Loader', async () => {
     const output = await loader('https://data.stackables.io/stackables-demo:prod/example1?introspection=a89751eb-ef0d-4663-9530-fa683b9d8483')
     expect(output).toMatchSnapshot()
 })
